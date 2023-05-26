75a2313657d4d9a0c2c806198af106ee5c7ea1c4
Tamas <tamas.fehertoi@gmail.com>
2023-05-26 19:50:44 +0200

    chart time period changeable

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import '../styles/Footer.css'
 
 function Footer () {
+ const date = new Date().toLocaleDateString("hu")
   return (
     <div className="footer">
-      This is Footer
+      {date}
     </div>
   )
 }
--- src/pages/ShowCoin.jsx
+++ src/pages/ShowCoin.jsx
@@ -7,13 +7,20 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
+import LoadingAnimation from "../components/LoadingAnimation";
+import "../styles/ShowCoin.css";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 export default function ShowCoin() {
   const params = useParams();
+  const [isLoading, setIsLoading] = useState(true);
   const [graphData, setGraphData] = useState();
+  const [period, setPeriod] = useState();
 
-  const url = `https://api.coingecko.com/api/v3/coins/${params.id}/market_chart?vs_currency=usd&days=365`;
+  const url = `https://api.coingecko.com/api/v3/coins/${params.id}/market_chart?vs_currency=usd&days=${period}`;
 
   const getSingleCoinData = async () => {
     const response = await fetch(url).then((response) => response.json());
@@ -26,27 +33,59 @@ export default function ShowCoin() {
 
   useEffect(() => {
     getSingleCoinData();
-  }, []);
+    setIsLoading(false);
+  }, [params.id, period]);
+
+  // Toggle bottoms
+
+  const handleChange = (event, newAlignment) => {
+    setPeriod(newAlignment);
+  };
 
   return (
     <>
-      <AreaChart
-        width={500}
-        height={400}
-        data={graphData}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="Date" />
-        <YAxis />
-        <Tooltip />
-        <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
-      </AreaChart>
+      {isLoading ? (
+        <LoadingAnimation />
+      ) : (
+        <div className="show-body">
+          <ToggleButtonGroup
+            color="primary"
+            value={period}
+            exclusive
+            onChange={handleChange}
+            aria-label="Platform"
+          >
+            <ToggleButton value="365">Year</ToggleButton>
+            <ToggleButton value="31">Months</ToggleButton>
+            <ToggleButton value="7">Week</ToggleButton>
+            <ToggleButton value="1">Day</ToggleButton>
+          </ToggleButtonGroup>
+          <ResponsiveContainer width="50%" height="50%">
+            <AreaChart
+              width={500}
+              height={400}
+              data={graphData}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="Date" />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="Price"
+                stroke="#8884d8"
+                fill="#8884d8"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+      )}
     </>
   );
 }
